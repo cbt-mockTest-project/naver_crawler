8a31bd1998972b424ea68f0a75107f7cf9a90212
eungwang <scm323@naver.com>
2023-01-12 01:48:03 +0900

    Feat: pwa 셋팅

--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+});
+const nextConfig = withPWA({
   reactStrictMode: true,
-}
+  swcMinify: true,
+  pwa: {
+    dest: "public",
+  },
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    domains: ["cbteungwangnestjs961203.s3.amazonaws.com"],
+  },
+  webpack: (config) => {
+    // 아래를 추가합니다.
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,33 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="description" content="네이버블로그 키워드 순위 크롤링 봇" />
+        <link
+          href="/icons/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link
+          href="/icons/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <meta name="msapplication-TileColor" content="#1990ff"></meta>
+        <link rel="manifest" href="/manifest.json" />
+
+        <meta name="theme-color" content="#ffffff" />
+        <meta
+          name="keywords"
+          content="산업안전기사,산업안전기사실기,산업안전기사실기CBT,전기기사,전기기사실기,전기기사실기CBT,전기기사실기단답형,전자문제집,모두CBT,정보처리기사,정보처리기사실기,정보처리기사실기CBT"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/png/favicon-32x32.png"
+        />
       </Head>
       <body>
         <Main />
